97d33bfb1023cbc0a205c6cfbc96c129b8b8b8de
Victor Neumann <victor.n.f.neumann@gmail.com>
2021-10-23 15:36:14 +0200

    user stories

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -7,7 +7,21 @@ class ParkingsController < ApplicationController
     @parkings = Parking.all
   end
 
+  def create
+    @parking = Parking.new(parking_params)
+    @parking.user = current_user
+    @parking.save
+    redirect_to parking_path(@parking)
+  end
+
   def new
     @parking = Parking.new
   end
+
+  private
+
+  def parking_params
+    params.require(:parking).permit(:title, :city, :street_address, :description, :category)
+  end
+
 end
